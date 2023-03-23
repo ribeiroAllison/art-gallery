3df3ea233dc79be50b28238fa49f4d34693d0c63
ribeiroAllison <allisonribeiro@gmail.com>
2023-03-23 15:54:05 -0300

    remove unnecessary file

--- public/assets/support.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom";
-
-export function getArtName (props) {
-    let {artName} = useParams()
-    const artArray = props.art[0];
-    const pageArt = artArray.filter(element=> element.name.split(' ').join('-').toLowerCase() === artName);
-    return `../../assets/${pageArt[0].name.split(' ').join('-').toLowerCase()}/gallery.jpg` 
-}
-
