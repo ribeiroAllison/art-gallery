ca3db261057d51f649016ce0cb186b680dcdad6c
ribeiroAllison <allisonribeiro@gmail.com>
2023-03-24 12:33:31 -0300

    add navigate to home function to Header logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,22 @@
+import { Outlet, useNavigate } from "react-router-dom"
 import logo from "../../../public/assets/img/galleria-logo.svg"
 import "../../../src/Global.css"
 import "./Header.css"
-import { Outlet } from "react-router-dom"
+
 
 
 export default function Header () {
 
+    const navigate = useNavigate();
+
+    const goHome = () => {
+        navigate("/")
+    }
+
     return(
         <div>
             <header id="header">
-                <img alt="galleria logo" src={logo}></img>
+                <img alt="galleria logo" src={logo} onClick={goHome}></img>
                 <h3>START SLIDESHOW</h3>
             </header>
             <Outlet />
